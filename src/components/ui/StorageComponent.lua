bc75723be9b5dcfe149d018eb26115871aadc2d9
Kenlnw <sorachat25500@gmail.com>
2026-05-07 17:25:13 +0900

    Update prototype town for implement a selling truck

--- src/components/ui/StorageComponent.lua
+++ src/components/ui/StorageComponent.lua
@@ -159,7 +159,7 @@ function StorageComponent:draw(player)
     local bg_padding = height_scale(16)
 
     -- background panel
-    set_color(0, 0, 0, 0.75)
+    set_color(0, 0, 0, 0.5)
     love.graphics.rectangle(
         "fill",
         self.x - bg_padding,
