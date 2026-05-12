7332df5e425e2871eb1c7327a5f52229328f02b8
Kenlnw <sorachat25500@gmail.com>
2026-05-13 00:08:39 +0900

    Fixed bug when deploy the game

--- src/components/ui/SlotComponent.lua
+++ src/components/ui/SlotComponent.lua
@@ -2,7 +2,7 @@ local SlotComponent = {}
 SlotComponent.__index = SlotComponent
 
 function SlotComponent:load(x, y, width, height, id, item)
-    TextBox = require("src.components.ui.TextboxComponent")
+    TextBox = require("src.components.ui.TextBoxComponent")
 
     local self = setmetatable({}, SlotComponent)
     self.x = x or 0
