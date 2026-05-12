f4e2b535483c6a6395c59e74ec3b854637534976
Kenlnw <sorachat25500@gmail.com>
2026-05-12 23:03:17 +0900

    change the console to false

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
     t.window.width = 1280
     t.window.height = 720
     t.window.fullscreen = true
-    t.window.fullscreentype = "desktop" 
-    t.console = true
+    t.window.fullscreentype = "desktop"
+    t.console = false
     t.window.title = "MindFarm"
 end
